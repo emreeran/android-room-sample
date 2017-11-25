8857e9ddf916b44c83e4ba161e79f48ff851464f
Emre Eran <emre.eran@gmail.com>
2017-11-25 22:49:46 +0300

    Added insertAll and listAll methods to UserDao

--- app/src/main/java/com/emreeran/android/roomsample/db/UserDao.java
+++ app/src/main/java/com/emreeran/android/roomsample/db/UserDao.java
@@ -2,14 +2,26 @@ package com.emreeran.android.roomsample.db;
 
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
 
 import com.emreeran.android.roomsample.vo.User;
+
+import java.util.List;
+
+import io.reactivex.Single;
 
 /**
  * Created by Emre Eran on 24.11.2017.
  */
 @Dao
 public interface UserDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(User user);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAll(User... user);
+
+    @Query("SELECT * FROM users")
+    Single<List<User>> listAll();
 }
