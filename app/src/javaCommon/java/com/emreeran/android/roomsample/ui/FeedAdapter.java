154840bc17eab925e0daf3040ea7c4a9ea08ea32
Emre Eran <emre.eran@gmail.com>
2018-06-13 20:55:21 +0900

    Passed click events by observables, added like action to view holder

--- app/src/javaCommon/java/com/emreeran/android/roomsample/ui/FeedAdapter.java
+++ app/src/javaCommon/java/com/emreeran/android/roomsample/ui/FeedAdapter.java
@@ -1,40 +1,50 @@
 package com.emreeran.android.roomsample.ui;
 
 import android.support.annotation.NonNull;
+import android.util.Pair;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
 import com.emreeran.android.roomsample.R;
-import com.emreeran.android.roomsample.ui.FeedItemViewHolder;
-import com.emreeran.android.roomsample.ui.common.ListAdapter;
 import com.emreeran.android.roomsample.db.vo.FeedItem;
+import com.emreeran.android.roomsample.ui.common.ListAdapter;
+
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
 
 /**
  * Created by Emre Eran on 9.05.2018.
  */
 public class FeedAdapter extends ListAdapter<FeedItem, FeedItemViewHolder> {
 
-    private View.OnClickListener mOnItemClickListener;
+    private PublishSubject<Integer> mItemClickSubject = PublishSubject.create();
+    private PublishSubject<Pair<Integer, Boolean>> mLikeClickSubject = PublishSubject.create();
+    private Observable<Integer> mItemClickEvent = mItemClickSubject;
+    private Observable<Pair<Integer, Boolean>> mLikeClickEvent = mLikeClickSubject;
 
     FeedAdapter() {
     }
 
-    public void setOnItemClickListener(View.OnClickListener onItemClickListener) {
-        mOnItemClickListener = onItemClickListener;
+    public Observable<Integer> getItemClickEvent() {
+        return mItemClickEvent;
+    }
+
+    public Observable<Pair<Integer, Boolean>> getLikeClickEvent() {
+        return mLikeClickEvent;
     }
 
     @NonNull
     @Override
     public FeedItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.feed_item, parent, false);
-        view.setOnClickListener(mOnItemClickListener);
-        return new FeedItemViewHolder(view);
+        return new FeedItemViewHolder(
+                LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.feed_item, parent, false)
+        );
     }
 
     @Override
     public void onBindViewHolder(@NonNull FeedItemViewHolder holder, int position) {
-        holder.setItem(mItems.get(position));
+        holder.setItem(mItems.get(position), mItemClickSubject, mLikeClickSubject);
     }
 
     @Override
--- app/src/javaCommon/java/com/emreeran/android/roomsample/ui/FeedItemViewHolder.java
+++ app/src/javaCommon/java/com/emreeran/android/roomsample/ui/FeedItemViewHolder.java
@@ -1,6 +1,7 @@
 package com.emreeran.android.roomsample.ui;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Pair;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -10,20 +11,31 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.emreeran.android.roomsample.R;
 import com.emreeran.android.roomsample.db.vo.FeedItem;
+import com.emreeran.android.roomsample.db.vo.LikeWithUser;
+
+import io.reactivex.subjects.PublishSubject;
 
 /**
  * Created by Emre Eran on 9.05.2018.
  */
 public class FeedItemViewHolder extends RecyclerView.ViewHolder {
 
+    private TextView mLikeCountTextView;
+
+    private int mLikeCount;
+
     FeedItemViewHolder(View itemView) {
         super(itemView);
     }
 
-    public void setItem(FeedItem item) {
+    public void setItem(FeedItem item,
+                        PublishSubject<Integer> itemClickSubject,
+                        PublishSubject<Pair<Integer, Boolean>> likeClickSubject
+    ) {
+        mLikeCountTextView = itemView.findViewById(R.id.like_count);
+
         TextView nameTextView = itemView.findViewById(R.id.name);
         TextView contentTextView = itemView.findViewById(R.id.content);
-        TextView likeCountTextView = itemView.findViewById(R.id.like_count);
         TextView commentCountTextView = itemView.findViewById(R.id.comment_count);
         ImageView userImageView = itemView.findViewById(R.id.user_image);
         ImageButton likeButton = itemView.findViewById(R.id.like_button);
@@ -32,26 +44,49 @@ public class FeedItemViewHolder extends RecyclerView.ViewHolder {
         nameTextView.setText(item.postWithUser.user.name);
         contentTextView.setText(item.postWithUser.post.content);
 
-        int likeCount = item.likes.size();
+        mLikeCount = item.likes.size();
+        setLikeCountText();
+
         int commentCount = item.comments.size();
-        String likeCountText = itemView.getResources().getString(R.string.like_count, likeCount);
         String commentCountText = itemView.getResources().getString(R.string.comment_count, commentCount);
-        likeCountTextView.setText(likeCountText);
+
         commentCountTextView.setText(commentCountText);
+        likeButton.setImageResource(R.drawable.ic_heart_outline);
+
+
+        for (LikeWithUser likeWithUser : item.likes) {
+            if (likeWithUser.isLiked) {
+                likeButton.setTag(true);
+                likeButton.setImageResource(R.drawable.ic_heart);
+                break;
+            }
+        }
 
         Glide.with(itemView)
                 .load(item.postWithUser.user.image)
                 .apply(new RequestOptions().circleCrop())
                 .into(userImageView);
 
+        itemView.setOnClickListener(v -> itemClickSubject.onNext(item.postWithUser.post.id));
+
         likeButton.setOnClickListener(v -> {
             boolean isLiked = v.getTag() != null && (boolean) v.getTag();
+            likeClickSubject.onNext(Pair.create(item.postWithUser.post.id, !isLiked));
+
             if (isLiked) {
                 ((ImageButton) v).setImageResource(R.drawable.ic_heart_outline);
+                mLikeCount--;
             } else {
                 ((ImageButton) v).setImageResource(R.drawable.ic_heart);
+                mLikeCount++;
             }
+            setLikeCountText();
             v.setTag(!isLiked);
         });
+    }
+
+    private void setLikeCountText() {
+        String likeCountText = itemView.getResources().getString(R.string.like_count, mLikeCount);
+        mLikeCountTextView.setText(likeCountText);
     }
 }
