0fbf75147d5dab7460fd3d72fd9d64c4823bf5c6
Emre Eran <emre.eran@gmail.com>
2018-06-13 20:56:09 +0900

    Used adapter observables for item click actions

--- app/src/javaDagger/java/com/emreeran/android/roomsample/ui/FeedFragment.java
+++ app/src/javaDagger/java/com/emreeran/android/roomsample/ui/FeedFragment.java
@@ -1,5 +1,7 @@
 package com.emreeran.android.roomsample.ui;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -13,13 +15,16 @@ import android.view.ViewGroup;
 
 import com.emreeran.android.roomsample.R;
 import com.emreeran.android.roomsample.db.dao.FeedDao;
-import com.emreeran.android.roomsample.db.vo.FeedItem;
+import com.emreeran.android.roomsample.db.dao.LikeDao;
+import com.emreeran.android.roomsample.db.entity.Like;
 import com.emreeran.android.roomsample.di.Injectable;
 
 import java.util.Objects;
+import java.util.concurrent.Callable;
 
 import javax.inject.Inject;
 
+import io.reactivex.Completable;
 import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
@@ -37,6 +42,9 @@ public class FeedFragment extends Fragment implements Injectable {
     FeedDao mFeedDao;
 
     @Inject
+    LikeDao mLikeDao;
+
+    @Inject
     NavigationController mNavigationController;
 
     private CompositeDisposable mDisposables;
@@ -50,28 +58,56 @@ public class FeedFragment extends Fragment implements Injectable {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Context context = getContext();
+        if (context == null) {
+            return;
+        }
+
         mDisposables = new CompositeDisposable();
+        SharedPreferences prefs =
+                context.getSharedPreferences("android-room-sample", Context.MODE_PRIVATE);
+        int currentUserId = prefs.getInt("logged_in_as", -1);
 
         RecyclerView feedView = Objects.requireNonNull(getView()).findViewById(R.id.feed);
 
         DividerItemDecoration itemDecoration =
-                new DividerItemDecoration(Objects.requireNonNull(getContext()), DividerItemDecoration.VERTICAL);
+                new DividerItemDecoration(context, DividerItemDecoration.VERTICAL);
         Drawable divider = getResources().getDrawable(R.drawable.feed_divider);
         itemDecoration.setDrawable(divider);
         feedView.addItemDecoration(itemDecoration);
 
         FeedAdapter adapter = new FeedAdapter();
-        adapter.setOnItemClickListener(v -> {
-            if (getActivity() != null) {
-                int itemPosition = feedView.getChildLayoutPosition(v);
-                FeedItem item = adapter.getListItem(itemPosition);
-                mNavigationController.navigateToPostDetail(item.postWithUser.post.id);
-            }
-        });
+        mDisposables.add(adapter.getItemClickEvent()
+                .subscribe(postId -> mNavigationController.navigateToPostDetail(postId)));
+        mDisposables.add(adapter.getLikeClickEvent().subscribe(pair -> {
+                    int postId = pair.first;
+                    boolean isLiked = pair.second;
+
+                    if (isLiked) {
+                        Like like = new Like(currentUserId, postId);
+                        mDisposables.add(Completable.fromCallable((Callable<Void>) () -> {
+                                    mLikeDao.insert(like);
+                                    return null;
+                                }).subscribeOn(Schedulers.io())
+                                        .observeOn(AndroidSchedulers.mainThread())
+                                        .subscribe()
+                        );
+                    } else {
+                        mDisposables.add(Completable.fromCallable((Callable<Void>) () -> {
+                                    mLikeDao.deleteByUserIdAndPostId(currentUserId, postId);
+                                    return null;
+                                }).subscribeOn(Schedulers.io())
+                                        .observeOn(AndroidSchedulers.mainThread())
+                                        .subscribe()
+                        );
+                    }
+                })
+        );
+
         feedView.setAdapter(adapter);
 
         mDisposables.add(
-                Flowable.fromCallable(mFeedDao::listFeedItems)
+                Flowable.fromCallable(() -> mFeedDao.listFeedItems(currentUserId))
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(adapter::replace)
